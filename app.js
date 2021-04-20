b56960c14dc29fa5c1a1be64218d1f17feeffbd5
Richard <82630326+richardsavala@users.noreply.github.com>
2021-04-19 23:45:23 -0600

    Add files via upload
    initial commit, solution incomplete

---
+++ app.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    let goal = target;
+    let temp = {};
+    for (let i = 0; i < 0; i++) {
+        temp.push(i);
+        console.log(temp);
+    }
+};
+
+const result = document.querySelector(".result");
+
+result.innerHTML = twoSum([2, 7, 11, 15], 9);
